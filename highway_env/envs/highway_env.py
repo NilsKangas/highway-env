166f60c50a48d0b47fb54a074c3eca5eea20435c
Nils Kangas <nils.kangas@gmail.com>
2023-02-07 23:16:39 +0100

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -87,14 +87,23 @@ class HighwayEnv(AbstractEnv):
         :param action: the last action performed
         :return: the corresponding reward
         """
-        rewards = self._rewards(action)
-        reward = sum(self.config.get(name, 0) * reward for name, reward in rewards.items())
-        if self.config["normalize_reward"]:
-            reward = utils.lmap(reward,
+        neighbours = self.road.network.all_side_lanes(self.vehicle.lane_index)
+        lane = self.vehicle.target_lane_index[2] if isinstance(self.vehicle, ControlledVehicle) \
+            else self.vehicle.lane_index[2]
+        scaled_speed = utils.lmap(self.vehicle.speed, self.config["reward_speed_range", [0, 1])
+     #   rewards = self._rewards(action)
+     #   reward = sum(self.config.get(name, 0) * reward for name, reward in rewards.items())
+         reward = \
+             + [self.config["collision_reward"] * self.vehicle.crashed \
+             + self.config["right_lane_reward"] * lane / max(len(neighbours) - 1, 1) \
+             + self.config["high_speed_reward"] * np.clip(scaled_speed, 0, 1)
+     #   if self.config["normalize_reward"]:
+        reward = utils.lmap(reward,
                                 [self.config["collision_reward"],
                                  self.config["high_speed_reward"] + self.config["right_lane_reward"]],
                                 [0, 1])
-        reward *= rewards['on_road_reward']
+    #    reward *= rewards['on_road_reward']
+        reward = 0 if not self.vehicle.on_road else reward                          
         return reward
 
     def _rewards(self, action: Action) -> Dict[Text, float]:
